fc7d62d6a3ae69edb51d3d7482409f41c18ab574
陈永利 <chenyongli@mananacare.com>
2023-01-29 11:26:22 +0800

    feat: 页脚添加备案号，页面基础内容补充

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: '后浪',
+    site_url: 'https://yooly.cc',
+    feed_url: 'https://yooly.cc/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
--- theme.config.js
+++ theme.config.js
@@ -3,8 +3,9 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
-      <a href="/feed.xml">RSS</a>
+      <time>{YEAR}</time> © 后浪
+      <a href="https://beian.miit.gov.cn" target="_blank">粤ICP备2021174590号</a>
+      {/*<a href="/feed.xml">RSS</a>*/}
       <style jsx>{`
         a {
           float: right;
